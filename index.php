fcc1181553ac4206b16d96a41de940adb4dd11ba
Anna Shannon (Student) <149623553+AnnaShannon2002@users.noreply.github.com>
2025-03-17 11:35:29 -0500

    copied from AnnaShannon2002

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <form action="validate.php" method="POST">
+            <p>
+                <h1>Please Login</h1><hr><br>
+            </p>
+            <p>
+                Username: <input type="text" name="user" id="user"><br>
+                Password: <input type="password" name="pass" id="pass"><br>
+                <input type="submit">
+            </p>
+    </body>
+</html>
