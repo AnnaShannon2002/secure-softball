6e6c1891e3f2cd5c3547e36e66b48d5a97015e36
Anna Shannon (Student) <149623553+AnnaShannon2002@users.noreply.github.com>
2025-03-17 11:38:15 -0500

    copied from AnnaShannon2002

---
+++ example.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        Login
+        <hr/>
+        TODO:
+        <ol>
+            <li>Create a login page that prevents access to the page below
+                for all but authenticated users.</li>
+            <li>Display authentication errors here, but do not give Mallory 
+                too much information!</li>
+            <li>Use a PHP File (not a webpage) to authenticate the user's 
+                credentials.</li>
+            <li>Use test_input or your own similar function to thwart Mallory.</li>
+        </ol>
+        <hr/>
+        <a href="lions2016.php">Championship Season</a>
+    </body>
+</html>
